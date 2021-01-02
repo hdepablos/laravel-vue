8854fd0bbf07aec1c489bd6873521a70c4dd3dff
hd <depabloshector@gmail.com>
2021-01-02 00:47:05 -0300

    first commit

---
+++ activated-docker.sh
@@ -0,0 +1,4 @@
+docker stop $(docker ps -a -q)
+
+./vendor/bin/sail up -d
+
---
+++ clear-cache.sh
@@ -0,0 +1,1 @@
+composer dump-autoload && php artisan clear-compiled && php artisan view:clear && php artisan route:clear && php artisan config:cache && php artisan cache:clear && php artisan optimize && php artisan route:cache && php artisan config:clear
